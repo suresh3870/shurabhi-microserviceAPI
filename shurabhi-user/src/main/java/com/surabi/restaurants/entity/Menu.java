a64c32b3cd46b89e43b3dbf6c0c6064c0ca8cd9c
suresh3870 <sksureshkumar88@gmail.com>
2021-11-24 23:38:40 +0530

    First with Postgres City V6

--- shurabhi-user/src/main/java/com/surabi/restaurants/entity/Menu.java
+++ shurabhi-user/src/main/java/com/surabi/restaurants/entity/Menu.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Menu {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer MenuId;
 
 
