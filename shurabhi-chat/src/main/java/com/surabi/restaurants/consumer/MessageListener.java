b7290260e0db511ba67ea9a792a5464f1fddd49a
suresh3870 <sksureshkumar88@gmail.com>
2021-11-28 19:38:49 +0530

    CHAT V5 works with Earliest one time

--- shurabhi-chat/src/main/java/com/surabi/restaurants/consumer/MessageListener.java
+++ shurabhi-chat/src/main/java/com/surabi/restaurants/consumer/MessageListener.java
@@ -17,42 +17,34 @@ import java.util.Properties;
 
 @Component
 public class MessageListener {
-
-    private KafkaConsumer<Integer, Message> consumer = null;
-    private String topic="shurabhi-chat";
-
-    public static final String KAFKA_SERVER_URL = "localhost";
-    public static final int KAFKA_SERVER_PORT = 9092;
-    public static final String CLIENT_ID = "Chat-consumer11";
-
-    public MessageListener() {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER_URL + ":" + KAFKA_SERVER_PORT);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");  //earliest
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-
-        consumer = new KafkaConsumer<>(props);
-
-    }
-
-
     public List<Message> doWork() {
+        String topic = "shurabhi-chat";
+        String KAFKA_SERVER_URL = "localhost";
+        int KAFKA_SERVER_PORT = 9092;
+        String CLIENT_ID = "Chat-consumer11";
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER_URL + ":" + KAFKA_SERVER_PORT);
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.setProperty(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");  //earliest
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<Integer, Message> consumer = new KafkaConsumer<Integer, Message>(props);
         consumer.subscribe(Collections.singletonList(topic));
-        ConsumerRecords<Integer, Message> records = consumer.poll(Duration.ofMillis(1000));
-        System.out.println("Number of message/s from records:->  "+records.count());
-        List<Message> msgs= new ArrayList<>();
-        for (ConsumerRecord<Integer, Message> record : records) {
-            msgs.add(record.value());
-           ////System.out.println(record.value().getTo());
-            System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
-            // System.out.println("Received message: " + record.value() + ") at offset " + record.offset());
+        while (true) {
+            ConsumerRecords<Integer, Message> records = consumer.poll(Duration.ofMillis(3000));
+            System.out.println("Number of message/s from records:->  " + records.count());
+            List<Message> msgs = new ArrayList<>();
+            for (ConsumerRecord<Integer, Message> record : records) {
+                msgs.add(record.value());
+                ////System.out.println(record.value().getTo());
+                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+                // System.out.println("Received message: " + record.value() + ") at offset " + record.offset());
+            }
+            return msgs;
         }
-       return msgs;
     }
 
 }
--- shurabhi-chat/src/main/java/com/surabi/restaurants/controller/ChatController.java
+++ shurabhi-chat/src/main/java/com/surabi/restaurants/controller/ChatController.java
@@ -3,14 +3,19 @@ package com.surabi.restaurants.controller;
 import com.surabi.restaurants.constants.KafkaConstants;
 import com.surabi.restaurants.consumer.MessageListener;
 import com.surabi.restaurants.model.Message;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+
+import static com.surabi.restaurants.constants.KafkaConstants.KAFKA_TOPIC;
 
 @RestController
 public class ChatController {
@@ -26,7 +31,7 @@ public class ChatController {
         message.setTimestamp(LocalDateTime.now().toString());
         try {
             System.out.println("Sending the message to kafka topic queue");
-            kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, message).get();
+            kafkaTemplate.send(KAFKA_TOPIC, message).get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
         }
