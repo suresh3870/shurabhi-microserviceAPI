3d3ff8933b5dff6b2d272ddf4de27f8e32390d24
suresh3870 <sksureshkumar88@gmail.com>
2021-11-28 17:20:08 +0530

    CHAT V5

--- shurabhi-chat/src/main/java/com/surabi/restaurants/controller/ChatController.java
+++ shurabhi-chat/src/main/java/com/surabi/restaurants/controller/ChatController.java
@@ -25,7 +25,7 @@ public class ChatController {
     public void sendMessage(@RequestBody Message message) {
         message.setTimestamp(LocalDateTime.now().toString());
         try {
-            //Sending the message to kafka topic queue
+            System.out.println("Sending the message to kafka topic queue");
             kafkaTemplate.send(KafkaConstants.KAFKA_TOPIC, message).get();
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
