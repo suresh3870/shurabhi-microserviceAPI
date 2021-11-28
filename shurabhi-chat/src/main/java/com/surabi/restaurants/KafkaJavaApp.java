d2a12346cf40ac90343c212e52d30194984dc011
suresh3870 <sksureshkumar88@gmail.com>
2021-11-28 08:55:55 +0530

    CHAT V1

---
+++ shurabhi-chat/src/main/java/com/surabi/restaurants/KafkaJavaApp.java
@@ -0,0 +1,11 @@
+package com.surabi.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaJavaApp.class, args);
+    }
+}
