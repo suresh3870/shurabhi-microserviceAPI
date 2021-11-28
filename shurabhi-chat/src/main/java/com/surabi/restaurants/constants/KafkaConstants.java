aac331efee00363978e26c7fe2fd21766d0ae3af
suresh3870 <sksureshkumar88@gmail.com>
2021-11-28 08:37:20 +0530

    FINAL

--- shurabhi-chat/src/main/java/com/surabi/restaurants/constants/KafkaConstants.java
+++
@@ -1,7 +0,0 @@
-package com.surabi.restaurants.constants;
-
-public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "shurabhi-chat";
-    public static final String GROUP_ID = "shurabhi-group";
-    public static final String KAFKA_BROKER = "localhost:9092";
-}
